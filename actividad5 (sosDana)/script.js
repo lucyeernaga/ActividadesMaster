b9eec8d6b0fedbaac26accfedafe49bbb43a194f
lucyeernaga <luciavicentecer004@alumnos.cei.es>
2024-11-15 12:06:59 +0100

    update actividad 5

--- actividad5 (sosDana)/script.js
+++
@@ -1,8 +0,0 @@
-// Obtener los elementos
-const menuHamburguesa = document.getElementById('menu-hamburguesa');
-const menu = document.getElementById('menu');
-
-// Función para alternar la visibilidad del menú
-menuHamburguesa.addEventListener('click', function() {
-    menu.classList.toggle('show');
-});
